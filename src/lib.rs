e54645b510174b84f16cba04647f0ab46ee40efd
Paul <pavel.zarubin@metalamp.io>
2022-11-18 17:12:40 +0300

    used formatter

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,126 @@
-use ureq;
-use serde::{Deserialize};
+use serde::Deserialize;
 use std::fmt;
-
+use ureq;
 
 #[derive(Deserialize)]
 pub struct TgGetMeResult {
     id: u64,
     is_bot: bool,
     first_name: String,
-    username: String
+    username: String,
 }
 
 impl fmt::Display for TgGetMeResult {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "id: {0},is_bot: {1}; first_name: {2}; username:{3};\n", self.id, self.is_bot, self.first_name, self.username)
-            }
-        }
+        write!(
+            f,
+            "id: {0},is_bot: {1}; first_name: {2}; username:{3};\n",
+            self.id, self.is_bot, self.first_name, self.username
+        )
+    }
+}
 
 #[derive(Deserialize)]
 pub struct TgResponse<T> {
     ok: bool,
-    pub result: T
+    pub result: T,
 }
 
 impl<T: fmt::Display> fmt::Display for TgResponse<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "ok: {0},\nresult: {1}", self.ok, self.result)
-            }
-        }
+    }
+}
 
 #[derive(Deserialize)]
-pub struct TgUser{
-    pub id: u64
-    }
+pub struct TgUser {
+    pub id: u64,
+}
 
 impl fmt::Display for TgUser {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "user_id: {0}", self.id)
-                }
-            }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "user_id: {0}", self.id)
+    }
+}
 
 #[derive(Deserialize)]
-pub struct TgMessage{
+pub struct TgMessage {
     pub message_id: u64,
-    pub from: Option<TgUser>
-        }
-
+    pub from: Option<TgUser>,
+}
 
 impl fmt::Display for TgMessage {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                match &self.from {
-                Some(user) => write!(f, "message_id: {0};message_from: {1}", self.message_id, user),
-                _ => write!(f, "message_id: {0};", self.message_id)
-                   }
-                }}
-    
-#[derive(Deserialize)]
-pub struct TgChat{
-    pub id: u64
+        match &self.from {
+            Some(user) => write!(
+                f,
+                "message_id: {0};message_from: {1}",
+                self.message_id, user
+            ),
+            _ => write!(f, "message_id: {0};", self.message_id),
+        }
+    }
 }
-                
+
+#[derive(Deserialize)]
+pub struct TgChat {
+    pub id: u64,
+}
+
 impl fmt::Display for TgChat {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "chat_id: {0}", self.id)
-                                }
-                            }
+    }
+}
 
 #[derive(Deserialize)]
-pub struct TgUpdate{
+pub struct TgUpdate {
     pub update_id: u64,
-    pub message: Option<TgMessage>//,
+    pub message: Option<TgMessage>, //,
 }
 
 impl fmt::Display for TgUpdate {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                match &self.message {
-                    Some(message) => write!(f, "update_Id: {0}; message: {1};", self.update_id, message),
-                    _ => write!(f, "update_id: {0};", self.update_id)
-                        }
-                    }}
-pub fn get_me(token: &str) -> Result<TgResponse<TgGetMeResult>,ureq::Error> {
-    let requset_string: &str = &(format!("{0}{1}{2}", "https://api.telegram.org/bot", token, "/getMe"));
-    let message: TgResponse<TgGetMeResult> = ureq::get(requset_string)
-                        .call()?
-                        .into_json()?;
+        match &self.message {
+            Some(message) => write!(f, "update_Id: {0}; message: {1};", self.update_id, message),
+            _ => write!(f, "update_id: {0};", self.update_id),
+        }
+    }
+}
+pub fn get_me(token: &str) -> Result<TgResponse<TgGetMeResult>, ureq::Error> {
+    let requset_string: &str =
+        &(format!("{0}{1}{2}", "https://api.telegram.org/bot", token, "/getMe"));
+    let message: TgResponse<TgGetMeResult> = ureq::get(requset_string).call()?.into_json()?;
     Ok(message)
 }
 
-
-pub fn get_updates(token: &str, update_id: &u64) -> Result<TgResponse<Vec<TgUpdate>>,ureq::Error> {
-    let requset_string: &str = &(format!("{0}{1}{2}", "https://api.telegram.org/bot", token, "/getUpdates"));
-    let update_id:&str = &format!("{}",update_id);
+pub fn get_updates(token: &str, update_id: &u64) -> Result<TgResponse<Vec<TgUpdate>>, ureq::Error> {
+    let requset_string: &str = &(format!(
+        "{0}{1}{2}",
+        "https://api.telegram.org/bot", token, "/getUpdates"
+    ));
+    let update_id: &str = &format!("{}", update_id);
     let messages = ureq::get(requset_string)
-                                .send_form(&[("offset",update_id),("timeout","10")])?
-                                .into_json()?;
+        .send_form(&[("offset", update_id), ("timeout", "10")])?
+        .into_json()?;
     Ok(messages)
 }
 
-pub fn send_echo(token: &str, message_id: u64, chat_id: u64) -> (){
-    let requset_string:&str = &(format!("{0}{1}{2}", "https://api.telegram.org/bot", token, "/copyMessage"));
-    let message_id_str:&str = &(format!("{}",message_id));
-    let chat_id_str:&str = &(format!("{}",chat_id));
-    let send_copy = ureq::get(requset_string)
-            .send_form(&[("chat_id",chat_id_str),("from_chat_id",chat_id_str),("message_id",message_id_str)]);
-    if send_copy.is_ok()
-        {
-            println!("message send")
-        }
-        else {println!("message not send")}
-
+pub fn send_echo(token: &str, message_id: u64, chat_id: u64) -> () {
+    let requset_string: &str = &(format!(
+        "{0}{1}{2}",
+        "https://api.telegram.org/bot", token, "/copyMessage"
+    ));
+    let message_id_str: &str = &(format!("{}", message_id));
+    let chat_id_str: &str = &(format!("{}", chat_id));
+    let send_copy = ureq::get(requset_string).send_form(&[
+        ("chat_id", chat_id_str),
+        ("from_chat_id", chat_id_str),
+        ("message_id", message_id_str),
+    ]);
+    if send_copy.is_ok() {
+        println!("message send")
+    } else {
+        println!("message not send")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,23 @@ fn main() {
     let mut update_id: u64 = 0;
     if check.is_ok() {
         loop {
-            
-        
-            let update_result = lib::get_updates(token,&update_id);
+            let update_result = lib::get_updates(token, &update_id);
 
-            match update_result
-            {
-                Ok(something) => for i in something.result
-                                                    {   
-                                                        let msg = i.message.unwrap();
-                                                        let m_id = msg.message_id;
-                                                        let c_id = msg.from.unwrap().id;
-                                                        lib::send_echo(token,m_id,c_id);
-                                                        update_id = i.update_id + 1 ;
+            match update_result {
+                Ok(something) => {
+                    for i in something.result {
+                        let msg = i.message.unwrap();
+                        let m_id = msg.message_id;
+                        let c_id = msg.from.unwrap().id;
+                        lib::send_echo(token, m_id, c_id);
+                        update_id = i.update_id + 1;
+                    }
+                }
 
-                                                    },
-
-                _ => println!("no updates")
+                _ => println!("no updates"),
             }
         }
-    } 
-        else {println!("bad token")}
-
+    } else {
+        println!("bad token")
+    }
 }
